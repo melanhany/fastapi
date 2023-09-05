ee98141a34e8d9233d1f631ca4855d55906228b9
Yermakhan <melanhany@gmail.com>
2023-09-06 03:22:49 +0500

    refactor dbquery for get_stores_by_employee_phone

--- poetryapi/crud.py
+++ poetryapi/crud.py
@@ -2,8 +2,7 @@ from sqlalchemy.orm import Session
 from . import models, schemas
 
 def get_stores_by_employee_phone(db: Session, phone_number: str):
-    stores = db.query(models.Store) \
-               .join(models.Employee) \
-               .filter(models.Employee.phone_number == phone_number) \
-               .all()
-    return stores
+    return db.query(models.Store) \
+                .join(models.Employee) \
+                .filter(models.Employee.phone_number == phone_number) \
+                .all()
