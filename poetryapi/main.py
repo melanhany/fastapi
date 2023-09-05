db2ad3fba939bfb908d6d3272ead4cf9cf729eff
Yermakhan <melanhany@gmail.com>
2023-09-06 03:25:45 +0500

    added dependency get_db and first api endpoint get_stores_for_employee

--- poetryapi/main.py
+++ poetryapi/main.py
@@ -1,9 +1,17 @@
-from typing import Union
-
-from fastapi import FastAPI
+from typing import Annotated, Union
+from fastapi import Depends, FastAPI, HTTPException, Query
+from sqlalchemy.orm import Session
+from . import crud, models, schemas
+from .database import SessionLocal, engine
 
 app = FastAPI()
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/")
 def read_root():
@@ -14,3 +22,15 @@ def read_root():
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
+@app.get("/stores/")
+def get_stores_for_employee(
+    phone_number: str, 
+    db: Session=Depends(get_db),
+    response_model=list[schemas.Store] 
+):
+    stores = crud.get_stores_by_employee_phone(db=db, phone_number=phone_number)
+
+    if not stores:
+        raise HTTPException(status_code=404, detail="No stores found for the provided employee phone number")
+
+    return stores
