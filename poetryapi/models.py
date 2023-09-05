4037fa265680e8b799ebea88d94c102766613893
Yermakhan <melanhany@gmail.com>
2023-09-05 17:13:28 +0500

    added Employee model

---
+++ poetryapi/models.py
@@ -0,0 +1,22 @@
+from enum import Enum as PythonEnum
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, Enum
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+
+Base = declarative_base()
+
+class Employee(Base):
+    __tablename__ = "employees"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255))
+    phone_number = Column(String(255))
+    store_id = Column(Integer, ForeignKey("stores.id"), nullable=False)
+    
+    store = relationship("Store", back_populates="employees")
+    
+
+    
+    
+    
