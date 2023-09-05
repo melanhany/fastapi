63bb927d54c5d0b08db8a4103ad4a9d1a2e9b59a
Yermakhan <melanhany@gmail.com>
2023-09-06 03:24:53 +0500

    added minimal schemas for the first get api

--- poetryapi/schemas.py
+++ poetryapi/schemas.py
@@ -1,4 +1,31 @@
 from pydantic import BaseModel
+from datetime import datetime
 
-class EmployeeStoreRequest(BaseModel):
+# class OrderBase(BaseModel):
+#     created_at: datetime
+#     closed_at: datetime
+    
+# class Order(OrderBase):
+#     id: int
+#     store_id: int
+#     author_id: int
+#     executor_id: int
+
+    
+class EmployeeBase(BaseModel):
+    name: str
     phone_number: str
+    
+class Employee(EmployeeBase):
+    id: int
+    store_id: int
+    # orders: list[Order] = []
+    
+
+class StoreBase(BaseModel):
+    name: str
+    
+class Store(StoreBase):
+    id: int
+    employees: list[Employee] = []
+
